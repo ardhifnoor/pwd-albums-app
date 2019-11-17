bc756361802d94be1be2da9d0de89223e210ec9b
Ardhi <ardhifnoor@outlook.com>
2019-11-17 13:32:58 +0700

    make Header usable anywhere

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
     return(
       <View style={ headerStyle.viewStyle }>
         <Text style={ headerStyle.textStyle }>
-            Albums
+            { this.props.title }
         </Text>
       </View>
     )
